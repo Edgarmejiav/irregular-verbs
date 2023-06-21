9fca546c39f8ad9992c673ab737028a3f97ec61c
edgarmejiav <edgarm_10@hotmail.com>
2023-06-20 22:39:46 -0500

    feat: add analytics

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { Analytics } from '@vercel/analytics/react';
+
 import App from './App'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
+      <Analytics mode={'production'} />;
+
   </React.StrictMode>,
 )
